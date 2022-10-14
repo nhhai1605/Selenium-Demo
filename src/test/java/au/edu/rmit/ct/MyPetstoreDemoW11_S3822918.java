b01d465a8269a07e567d5f488671c44837351de1
Nguyen Hoang Hai (S3822918) <68266079+nhhai1605@users.noreply.github.com>
2022-10-14 18:31:02 +1100

    Update handling exceptions

--- src/test/java/au/edu/rmit/ct/MyPetstoreDemoW11_S3822918.java
+++ src/test/java/au/edu/rmit/ct/MyPetstoreDemoW11_S3822918.java
@@ -43,7 +43,7 @@ class MyPetstoreDemoW11_S3822918
     //Assume that the stock here can be updated ==>
     //So, we still can place an order with "Back ordered." and "<number> in stock." but only if <number> is larger than 0.
     //If <number> is 0, we will throw an exception.
-    void checkStock() throws Exception
+    void checkStock()
     {
         String backOrdered = "Back ordered.";
         String inStock = "in stock.";
@@ -51,7 +51,8 @@ class MyPetstoreDemoW11_S3822918
         WebElement webElement = myDriver.findElement(By.xpath(String.format("//td[contains(text(), '%s')]|//td[contains(text(), '%s')]", inStock, backOrdered)));
         if(webElement.getText().equals(wrongStockFormat))
         {
-            throw new Exception("Test Failed: The stock format cannot be '0 in stock.'");
+//            throw new Exception("Test Failed: The stock format cannot be '0 in stock.'");
+            fail("Test Failed: The stock format cannot be '0 in stock.'");
         }
     }
 
@@ -60,34 +61,39 @@ class MyPetstoreDemoW11_S3822918
     @DisplayName("Check the pet name, price and check if there is stock for one pet of your choice.")
     void checkAnotherPet() throws Exception {
         String manxURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-15";
-        WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
-        myDriver.get(manxURL);
-        wait.until(ExpectedConditions.urlToBe(manxURL));
-
         String itemId = "EST-15";
         String petName = "With tail Manx";
         String petBreed = "Manx";
         String petPrice = "$23.50";
 
-        WebElement webElement;
+        WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
+        myDriver.get(manxURL);
+        try {
+            wait.until(ExpectedConditions.urlToBe(manxURL));
+            WebElement webElement;
 
-        webElement = myDriver.findElement(By.xpath(String.format("//b[contains(text(), '%s')]", itemId)));
-        assertEquals(itemId, webElement.getText());
+            webElement = myDriver.findElement(By.xpath(String.format("//b[contains(text(), '%s')]", itemId)));
+            assertEquals(itemId, webElement.getText());
 
-        webElement = myDriver.findElement(By.xpath(String.format("//font[contains(text(), '%s')]", petBreed)));
-        assertEquals(petName, webElement.getText());
+            webElement = myDriver.findElement(By.xpath(String.format("//font[contains(text(), '%s')]", petBreed)));
+            assertEquals(petName, webElement.getText());
 
-        webElement = myDriver.findElement(By.xpath(String.format("//td[contains(text(), '%s')]", petBreed)));
-        assertEquals(petBreed, webElement.getText());
+            webElement = myDriver.findElement(By.xpath(String.format("//td[contains(text(), '%s')]", petBreed)));
+            assertEquals(petBreed, webElement.getText());
 
-        //Please read the comment above the checkStock() function for more details about this function.
-        checkStock();
+            //Please read the comment above the checkStock() function for more details about this function.
+            checkStock();
 
-        webElement = myDriver.findElement(By.xpath(String.format("//td[contains(text(), '%s')]", petPrice)));
-        assertEquals(petPrice, webElement.getText());
+            webElement = myDriver.findElement(By.xpath(String.format("//td[contains(text(), '%s')]", petPrice)));
+            assertEquals(petPrice, webElement.getText());
 
+            print("------ Checking for another pet passed -------");
+        }
+        catch (Exception e)
+        {
+            fail("Checking for another pet failed");
+        }
 
-        print("------ Checking for another pet passed -------");
 
     }
 
@@ -110,45 +116,52 @@ class MyPetstoreDemoW11_S3822918
         WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
         WebElement webElement;
         float actualCost = 0f;
-
         myDriver.get(fishURL);
-        wait.until(ExpectedConditions.urlToBe(fishURL));
-        checkStock();
-        webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$5.29')]"));
-        actualCost += Float.parseFloat(webElement.getText().substring(1)) * 3;
-
-        myDriver.get(catURL);
-        wait.until(ExpectedConditions.urlToBe(catURL));
-        checkStock();
-        webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$93.50')]"));
-        actualCost += Float.parseFloat(webElement.getText().substring(1)) * 2;
-
-        myDriver.get(dogURL);
-        wait.until(ExpectedConditions.urlToBe(dogURL));
-        checkStock();
-        webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$18.50')]"));
-        actualCost += Float.parseFloat(webElement.getText().substring(1)) * 1;
-
-        for (int i = 0; i < 3; i++)
+        try
         {
-            myDriver.get(fishActionURL);
-            wait.until(ExpectedConditions.urlToBe(fishActionURL));
+            wait.until(ExpectedConditions.urlToBe(fishURL));
+            checkStock();
+            webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$5.29')]"));
+            actualCost += Float.parseFloat(webElement.getText().substring(1)) * 3;
+
+            myDriver.get(catURL);
+            wait.until(ExpectedConditions.urlToBe(catURL));
+            checkStock();
+            webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$93.50')]"));
+            actualCost += Float.parseFloat(webElement.getText().substring(1)) * 2;
+
+            myDriver.get(dogURL);
+            wait.until(ExpectedConditions.urlToBe(dogURL));
+            checkStock();
+            webElement = myDriver.findElement(By.xpath("//td[contains(text(), '$18.50')]"));
+            actualCost += Float.parseFloat(webElement.getText().substring(1)) * 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                myDriver.get(fishActionURL);
+                wait.until(ExpectedConditions.urlToBe(fishActionURL));
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                myDriver.get(catActionURL);
+                wait.until(ExpectedConditions.urlToBe(catActionURL));
+            }
+            for(int i = 0; i < 1; i++)
+            {
+                myDriver.get(dogActionURL);
+                wait.until(ExpectedConditions.urlToBe(dogActionURL));
+            }
+            webElement = myDriver.findElement(By.xpath(String.format("//*[contains(text(), 'Sub Total')]")));
+            print(String.format("Actual cost: %.2f", actualCost));
+            print("Expected cost: " + webElement.getText().substring(12));
+            assertEquals(String.format("Sub Total: $%.2f", actualCost), webElement.getText(), "The subtotal cost is not correct");
+            print("------ Checking for starting a menagerie passed -------");
         }
-        for (int i = 0; i < 2; i++)
+        catch (Exception e)
         {
-            myDriver.get(catActionURL);
-            wait.until(ExpectedConditions.urlToBe(catActionURL));
+            fail("Start a menagerie failed");
         }
-        for(int i = 0; i < 1; i++)
-        {
-            myDriver.get(dogActionURL);
-            wait.until(ExpectedConditions.urlToBe(dogActionURL));
-        }
-        webElement = myDriver.findElement(By.xpath(String.format("//*[contains(text(), 'Sub Total')]")));
-        print(String.format("Actual cost: %.2f", actualCost));
-        print("Expected cost: " + webElement.getText().substring(12));
-        assertEquals(String.format("Sub Total: $%.2f", actualCost), webElement.getText(), "The subtotal cost is not correct");
-        print("------ Checking for starting a menagerie passed -------");
+
     }
 
 
--- src/test/java/au/edu/rmit/ct/TryBookingTest_S3822918.java
+++ src/test/java/au/edu/rmit/ct/TryBookingTest_S3822918.java
@@ -37,7 +37,7 @@ class TryBookingTest_S3822918
     @Order(1)
     @DisplayName("Enter TryBooking Website")
 
-    void enterTryBookingWebsite()
+    void enterTryBookingWebsite() throws Exception
     {
         String url = "https://www.trybooking.com/";
         WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
@@ -56,7 +56,7 @@ class TryBookingTest_S3822918
     @Order(2)
     @RepeatedTest(10) //Repeat 10 times here because of the randomness of each time loading the page.
     @DisplayName("Check there are 12 unique events near me")
-    void checkUniqueEvents()
+    void checkUniqueEvents() throws Exception
     {
         String url = "https://www.trybooking.com/book/search";
         WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
@@ -71,36 +71,46 @@ class TryBookingTest_S3822918
         }
         int count = 0;
 
-        //Because there are multiple events with images, so we need to wait for all elements to fully load.
-        List<WebElement> webElements = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@id='nearby-events-container']/div")));
-        List<String> eventDatesAndLocations = new ArrayList<>();
-        print("Total Events: " + webElements.size());
-        for(WebElement webElement : webElements)
+        try
         {
-            String eventName = webElement.getAttribute("data-event-name");
-            String eventDate = webElement.findElements(By.xpath(".//span")).get(0).getText();
-            String eventLocation = webElement.findElements(By.xpath(".//span")).get(1).getText();
-            String eventDateAndLocation = eventDate + " " + eventLocation;
-            eventDateAndLocation = eventDateAndLocation.replaceAll("[^a-zA-Z0-9\\s]", "");
-            if (!eventDatesAndLocations.contains(eventDateAndLocation))
+            //Because there are multiple events with images, so we need to wait for all elements to fully load.
+            List<WebElement> webElements = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@id='nearby-events-container']/div")));
+            List<String> eventDatesAndLocations = new ArrayList<>();
+            print("Total Events: " + webElements.size());
+            for(WebElement webElement : webElements)
             {
-                //Based on my observation, most of the duplicate events may have completely different name, but the date and location are the same.
-                //So, I will only base on the date and location to determine if the event is duplicate or not.
-                count++;
-                eventDatesAndLocations.add(eventDateAndLocation);
+                String eventName = webElement.getAttribute("data-event-name");
+                String eventDate = webElement.findElements(By.xpath(".//span")).get(0).getText();
+                String eventLocation = webElement.findElements(By.xpath(".//span")).get(1).getText();
+                String eventDateAndLocation = eventDate + " " + eventLocation;
+                eventDateAndLocation = eventDateAndLocation.replaceAll("[^a-zA-Z0-9\\s]", "");
+                if (!eventDatesAndLocations.contains(eventDateAndLocation))
+                {
+                    //Based on my observation, most of the duplicate events may have completely different name, but the date and location are the same.
+                    //So, I will only base on the date and location to determine if the event is duplicate or not.
+                    count++;
+                    eventDatesAndLocations.add(eventDateAndLocation);
+                }
+
             }
+            print("Unique Event Number: " + count);
+            assertTrue(count >= 12);
+            print("------ Checking for 12 unique events passed -------");
 
         }
-        print("Unique Event Number: " + count);
-        assertTrue(count >= 12);
-        print("------ Checking for 12 unique events passed -------");
+        catch (Exception e)
+        {
+            fail("Check there are 12 unique events near me failed");
+        }
+
 
     }
 
     @Test
     @Order(3)
     @DisplayName("Check booking into an event")
-    void checkBookingAnEvent() throws Exception {
+    void checkBookingAnEvent() throws Exception
+    {
         String url = "https://www.trybooking.com/CDKGX";
         WebDriverWait wait = new WebDriverWait(myDriver, Duration.ofMillis(3000));
         myDriver.get(url);
@@ -113,88 +123,98 @@ class TryBookingTest_S3822918
         {
             fail("Enter RMIT Golden Doughnut Award Ceremony site failed");
         }
-        WebElement webElement =  wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='book-button-top']/div/div/button")));
-        webElement.click();
 
-        webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(), 'Platypi')]")));
-        webElement = webElement.findElement(By.xpath("./../.."));
-        webElement = webElement.findElement(By.xpath(".//a"));
-        webElement.click();
+        try
+        {
+            WebElement webElement =  wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='book-button-top']/div/div/button")));
+            webElement.click();
 
-        webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@class='step plus']")));
-        webElement.click();
-        Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
-        webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tickets-next-button")));
-        webElement.click();
+            webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(), 'Platypi')]")));
+            webElement = webElement.findElement(By.xpath("./../.."));
+            webElement = webElement.findElement(By.xpath(".//a"));
+            webElement.click();
 
-
-        WebElement idInputText = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Student ID is required.']")));
-        idInputText.sendKeys("s3822918");
-        webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-day')]"));
-        webElement.click();
-        Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
-        webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='21']"));
-        webElement.click();
+            webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@class='step plus']")));
+            webElement.click();
+            Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
+            webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("tickets-next-button")));
+            webElement.click();
 
 
-        webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-month')]"));
-        webElement.click();
-        Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
-        webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='Oct']"));
-        webElement.click();
-
-        webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-year')]"));
-        webElement.click();
-        Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
-        webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='2022']"));
-        webElement.click();
+            WebElement idInputText = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@title='Student ID is required.']")));
+            idInputText.sendKeys("s3822918");
+            webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-day')]"));
+            webElement.click();
+            Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
+            webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='21']"));
+            webElement.click();
 
 
-        webElement = myDriver.findElement(By.xpath(".//select[@title='Is this your last semester at RMIT? is required.']"));
-        webElement = webElement.findElement(By.xpath("./.."));
-        webElement = webElement.findElement(By.xpath(".//div[contains(@class, 'ss-main')]"));
-        webElement.click();
-        Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
-        webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='Yes']"));
-        webElement.click();
+            webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-month')]"));
+            webElement.click();
+            Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
+            webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='Oct']"));
+            webElement.click();
 
-        Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
-        webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='data-entry-next-button']")));
-        webElement.click();
+            webElement = myDriver.findElement(By.xpath("//div[contains(@class, 'date-year')]"));
+            webElement.click();
+            Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
+            webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='2022']"));
+            webElement.click();
 
-        webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='txtFirstName']")));
-        webElement.sendKeys("Hai");
-        webElement = myDriver.findElement(By.xpath("//input[@id='txtLastName']"));
-        webElement.sendKeys("Nguyen");
 
-        webElement = myDriver.findElement(By.xpath("//select[@id='drpCountry']"));
-        webElement.click();
-        Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
-        webElement = webElement.findElement(By.xpath("//option[@value='VN']"));
-        webElement.click();
+            webElement = myDriver.findElement(By.xpath(".//select[@title='Is this your last semester at RMIT? is required.']"));
+            webElement = webElement.findElement(By.xpath("./.."));
+            webElement = webElement.findElement(By.xpath(".//div[contains(@class, 'ss-main')]"));
+            webElement.click();
+            Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
+            webElement = webElement.findElement(By.xpath("//*[@class='ss-option' and text()='Yes']"));
+            webElement.click();
 
-        webElement = myDriver.findElement(By.xpath("//input[@id='txtEmailAddress']"));
-        webElement.sendKeys("nhhai1605@gmail.com");
-        webElement = myDriver.findElement(By.xpath("//input[@id='txtConfirmEmailAddress']"));
-        webElement.sendKeys("nhhai1605@gmail.com");
-        webElement = myDriver.findElement(By.xpath("//label[contains(text(), 'Remember my booking details on this computer for next time.')]"));
-        JavascriptExecutor js = (JavascriptExecutor) myDriver;
-        js.executeScript("arguments[0].scrollIntoView();", webElement); //I have to scroll down to the checkbox to tick it.
-        wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
-        webElement = myDriver.findElement(By.xpath("//button[@id='btn-purchase-sm']"));
-        Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
-        webElement.click();
+            Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
+            webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='data-entry-next-button']")));
+            webElement.click();
 
-        Thread.sleep(1000);
+            webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='txtFirstName']")));
+            webElement.sendKeys("Hai");
+            webElement = myDriver.findElement(By.xpath("//input[@id='txtLastName']"));
+            webElement.sendKeys("Nguyen");
+
+            webElement = myDriver.findElement(By.xpath("//select[@id='drpCountry']"));
+            webElement.click();
+            Thread.sleep(1000); //Sleep here so that the dropdown is fully loaded.
+            webElement = webElement.findElement(By.xpath("//option[@value='VN']"));
+            webElement.click();
+
+            webElement = myDriver.findElement(By.xpath("//input[@id='txtEmailAddress']"));
+            webElement.sendKeys("nhhai1605@gmail.com");
+            webElement = myDriver.findElement(By.xpath("//input[@id='txtConfirmEmailAddress']"));
+            webElement.sendKeys("nhhai1605@gmail.com");
+            webElement = myDriver.findElement(By.xpath("//label[contains(text(), 'Remember my booking details on this computer for next time.')]"));
+            JavascriptExecutor js = (JavascriptExecutor) myDriver;
+            js.executeScript("arguments[0].scrollIntoView();", webElement); //I have to scroll down to the checkbox to tick it.
+            wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
+            webElement = myDriver.findElement(By.xpath("//button[@id='btn-purchase-sm']"));
+            Thread.sleep(1000); //Sleep for checking the input before the automation continue to next page.
+            webElement.click();
+
+            Thread.sleep(1000);
+        }
+        catch (Exception e)
+        {
+            fail("Check booking into an event failed");
+        }
+
         try
         {
             //If the title is not "Confirmation | TryBooking Australia", it means that the booking is not successful.
             wait.until(ExpectedConditions.titleIs("Confirmation | TryBooking Australia"));
+            print("------ Checking for booking into an event passed -------");
+
         } catch (Exception e)
         {
-            fail("Booking failed");
+            fail("Check booking into an event failed");
         }
-        print("------ Checking for booking into an event passed -------");
 
     }
 
